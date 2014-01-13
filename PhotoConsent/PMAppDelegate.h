4ee91e25c2409e8275693250d6f20e89cd5a3261
alex rafferty <alexmr@btopenworld.com>
2014-01-13 09:54:04 +0000

    Initial Commit

---
+++ PhotoConsent/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  PhotoConsent
+//
+//  Created by Alex Rafferty on 13/01/2014.
+//  Copyright (c) 2014 PM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoConsent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoConsent
+//
+//  Created by Alex Rafferty on 13/01/2014.
+//  Copyright (c) 2014 PM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
